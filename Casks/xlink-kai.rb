43503e6e73ac1e0a418eae2aee202dae091400c3
Mason Thompson <72317541+MasonT8198@users.noreply.github.com>
2022-07-31 21:16:29 -0400

    xlink-kai.rb: Update description, sha256 hash, and url to pull

--- Casks/xlink-kai.rb
+++ Casks/xlink-kai.rb
@@ -1,11 +1,17 @@
 cask "xlink-kai" do
   version "7.4.43,606669993"
-  sha256 "e75c5f002ccc2071919f66836e0aa504cce1ab910d96ae587bc1e61da5c4ad0c"
+  sha256 "01ad053900d624e7b98507a7F2d2f15723bd40e08ea70dceee43057c230a0631"
 
-  url "https://github.com/Team-XLink/releases/releases/download/#{version.before_comma}/XLinkKai-#{version.before_comma}-#{version.after_comma}-macOS.dmg",
+  url "https://github.com/Team-XLink/releases/releases/download/#{version.before_comma}/XLinkKai-#{version.before_comma}-#{version.after_comma}-macOS_x86_64.dmg",
       verified: "github.com/Team-XLink/releases/"
   name "XLink Kai"
-  desc "Play LAN Games online with XLink Kai!"
+  desc "XLink Kai is a global gaming network - bringing together Xbox, Xbox 360, 
+ Xbox One, Xbox Series X/S, Playstation 2, Playstation 3, Playstation 4,
+ Playstation 5, PSP, PS Vita, Gamecube, Wii, Wiiu, and Switch, users, in 
+ one integrated community. It is software running on your PC or Macintosh 
+ that allows you to  play system-link enabled games online for free. 
+ Kai is the only service that is not console specific, and boasts one of
+ the friendliest communities available."
   homepage "https://www.teamxlink.co.uk/"
 
   livecheck do
